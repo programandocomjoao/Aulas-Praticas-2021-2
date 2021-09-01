58e3fda483ff07f9f4345b9152bbe354748745bc
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2021-08-31 21:45:24 -0300

    Add files via upload

---
+++ FDASW(429-20) TER Noite/2021-08-31/meuControlador.js
@@ -0,0 +1,9 @@
+app.controller("meuControlador",
+    function($scope) {
+        $scope.nome = "José Maria";
+        $scope.idade = 68;
+        $scope.peso = 79.9;
+        $scope.altura = 1.83;
+        $scope.sexo = 'M';
+    }
+);
