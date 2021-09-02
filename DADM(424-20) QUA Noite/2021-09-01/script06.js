791786ee4f80de2b6c4b7824e3a1d61174b3d109
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2021-09-01 23:30:47 -0300

    Add files via upload

---
+++ DADM(424-20) QUA Noite/2021-09-01/script06.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var data = require('./data');
+
+http.createServer(
+    function(req, res) {
+        res.writeHead(200, {'Content-type':'text/html; charset=utf-8'});    
+        
+        res.write('<!DOCTYPE html>');
+        res.write('<html lang="pt-br">');
+        res.write('<head>');
+        res.write('<title>Aula de Node.js</title>');
+        res.write('</head>');
+        res.write('<body>');
+        res.write('<h1>Sejam Bem-vindo(a)</h1>');
+        res.write(`<p>Hoje é ${data.retornarDia()}/${data.retornarMes()}/${data.retornarAno()} e no momento são ${data.retornarHora()}:${data.retornarMinuto()}.</p>`);
+        res.write('</body>');
+        res.write('</html>');
+
+        res.end();
+    }
+).listen(8080);
+
+console.log('Servidor iniciado na porta 8080. Pressione Ctrl + C para encerrar.');
---
+++ DADM(424-20) QUA Noite/2021-09-01/script08.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(
+    function(req, res) {
+        res.writeHead(200, {'Content-type':'text/html; charset=utf-8'});    
+        
+        var endereco = 'http://localhost:8080' + req.url;
+        var dados = url.parse(endereco, true);
+        var args = dados.query;
+
+        res.write('<!DOCTYPE html>');
+        res.write('<html lang="pt-br">');
+        res.write('<head>');
+        res.write('<title>Aula de Node.js</title>');
+        res.write('</head>');
+        res.write('<body>');
+        res.write('<h1>Seja Bem-vindo(a)</h1>');
+        res.write(`<p>Domínio: ${dados.host}</p>`);
+        res.write(`<p>Subdomínio: ${dados.pathname}</p>`);
+        res.write(`<p>Argumentos: ${dados.search}</p>`);
+        res.write(`<p>Nome: ${args.nome}</p>`);
+        res.write(`<p>Sobrenome: ${args.sobrenome}</p>`);
+        res.write('</body>');
+        res.write('</html>');
+
+        res.end();
+    }
+).listen(8080);
+
+console.log('Servidor iniciado na porta 8080. Pressione Ctrl + C para encerrar.');
