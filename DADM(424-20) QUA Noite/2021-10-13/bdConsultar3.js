3f45fea9dd0741d0f9a8952c04ec46a340738243
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2021-10-13 21:42:50 -0300

    Add files via upload

---
+++ DADM(424-20) QUA Noite/2021-10-13/bdConsultar3.js
@@ -0,0 +1,33 @@
+var conexao = require('./bdConectaAgenda');
+
+conexao.connect(
+    function(err) {
+        if (err)
+            throw err;
+        else {
+            var operacao = "SELECT COUNT(id) AS qtd FROM pessoa";
+
+            conexao.query(operacao,
+                function(err, result, fields) {
+                    if (err)
+                        throw err;
+                    else {
+                        console.log(`A quantidade de pessoas é ${result[0].qtd}`);
+                        var qtd = result[0].qtd;
+                        var operacao2 = "SELECT * FROM pessoa";
+
+                        conexao.query(operacao2,
+                            function(err, result, fields) {
+                                if (err)
+                                    throw err;
+                                else
+                                    for (var i = 0; i < qtd; i++)
+                                        console.log(`| ${result[i].id} | ${result[i].nome} | ${result[i].email} |`);
+                            }
+                        );
+                    }
+                }
+            ); 
+        }      
+    }
+);
