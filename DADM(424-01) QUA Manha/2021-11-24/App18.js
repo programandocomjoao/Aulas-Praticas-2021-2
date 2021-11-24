d360ed36c610917b046fab3ef97b01895d08c320
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2021-11-24 18:22:46 -0300

    Add files via upload

--- DADM(424-01) QUA Manha/2021-11-24/App18.js
+++ DADM(424-01) QUA Manha/2021-11-24/App18.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './componentes/Home';
+import Midias from './componentes/Midias';
 
+const Stack = createStackNavigator();
+
+const App = () => { 
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Midias" component={Midias} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
